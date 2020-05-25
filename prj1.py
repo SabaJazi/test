2ee9cbc920bac5eb63ee4c43dc2ce083d0f70f02
SabaJazi <saba.yousefian72@gmail.com>
2020-05-24 21:32:09 -0500

    commit 5:widget buttons changed/layout type changed from grid to place

--- prj1.py
+++ prj1.py
@@ -1,10 +1,8 @@
-#print("test hellooooo")
-#print("changes on the local computer 1")
-#-------------------------------------------libraries--------------------------------
+# -------------------------------------------libraries--------------------------------
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
-#import descartes
+# import descartes
 import pyodbc
 import numpy as np
 import sys
@@ -13,11 +11,13 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
-#-------------------------making the window------------------------
+# -------------------------making the window------------------------
 
-my_window=Tk()
-my_canvas=Canvas(my_window, width=300, height=300,background='white')
-my_canvas.grid(row=0, column=0)
+my_window = Tk()
+# my_canvas = Canvas(my_window, width=500, height=200, background='white')
+my_canvas = Canvas(my_window, background='white')
+# my_canvas.grid(row=3, column=0)
+my_window.geometry("500x500")
 
 
 # -------------function run_query()---------called by button 1--------
@@ -28,41 +28,85 @@ my_canvas.grid(row=0, column=0)
 def run_query():
     variable = my_combobox.get()
     (result1, result2) = choices.get(variable, ('default1', 'default2'))
+
     print(result1)
     print(result2)
+
+#----------------listbox------------------------
+
+# ---------------------- label ------------------------
+combo_label = ttk.Label(my_window, text="select diseases of: ")
+# combo_label.grid(row=0,sticky="")
+combo_label.place(relwidth=0.7, relheight=0.3, relx=0.3, rely=0.2)
+# lable_sep=ttk.Separator(my_window,orient="horizontal")
+# lable_sep.grid(row=0, column=0)
 
 # ----------------------combo box------------------------
 
 
-deases_lst = ['deases1', 'deases2', 'deases3', 'deases4', 'deases5', 'deases6'
-              , 'deases7', 'deases8', 'deases9', 'deases10', 'deases11', 'deases12'
-              , 'deases13', 'deases14', 'deases15', 'deases16', 'deases17', 'deases18']
+deases_lst = ['infectious and parasitic diseases',
+              'neoplasms',
+              'endocrine, nutritional and metabolic diseases, and immunity disorders',
+              'diseases of the blood and blood-forming organs',
+              'mental disorders',
+              'diseases of the nervous system',
+              'diseases of the sense organs',
+              'diseases of the circulatory system(Heart Related)',
+              'diseases of the respiratory system',
+              'diseases of the digestive system',
+              'diseases of the genitourinary system',
+              'complications of pregnancy, childbirth, and the puerperium',
+              'diseases of the skin and subcutaneous tissue',
+              'diseases of the musculoskeletal system and connective tissue',
+              'congenital anomalies',
+              'certain conditions originating in the perinatal period',
+              'symptoms, signs, and ill-defined conditions',
+              'injury and poisoning']
 
-choices = {'deases1': (1, 1399), 'deases2': (140, 2399), 'deases3': (240, 2799),
-           'deases4': (280, 2899),'deases5': (290, 3199),'deases6': (320, 3599),
-           'deases7': (360, 3899),'deases8': (390, 4599),'deases9': (460, 5199),
-           'deases10': (520, 5799),'deases11': (580, 6299),'deases12': (630, 6799),
-           'deases13': (680, 7099),'deases14': (710, 7399),'deases15': (740, 7599),
-           'deases16': (760, 7799),'deases17': (780, 7999),'deases18': (800, 9999)
+choices = {'infectious and parasitic diseases': (1, 1399),
+           'neoplasms': (140, 2399),
+           'endocrine, nutritional and metabolic diseases, and immunity disorders': (240, 2799),
+           'diseases of the blood and blood-forming organs': (280, 2899),
+           'mental disorders': (290, 3199),
+           'diseases of the nervous system': (320, 3599),
+           'diseases of the sense organs': (360, 3899),
+           'diseases of the circulatory system(Heart Related)': (390, 4599),
+           'diseases of the respiratory system': (460, 5199),
+           'diseases of the digestive system': (520, 5799),
+           'diseases of the genitourinary system': (580, 6299),
+           'complications of pregnancy, childbirth, and the puerperium': (630, 6799),
+           'diseases of the skin and subcutaneous tissue': (680, 7099),
+           'diseases of the musculoskeletal system and connective tissue': (710, 7399),
+           'congenital anomalies': (740, 7599),
+           'certain conditions originating in the perinatal period': (760, 7799),
+           'symptoms, signs, and ill-defined conditions': (780, 7999),
+           'injury and poisoning': (800, 9999)
            }
 
 my_str_var = tk.StringVar()
 my_combobox = ttk.Combobox(my_window, textvariable=my_str_var, values=deases_lst)
-my_combobox.grid(row=0, column=0)
+#my_combobox.grid(row=1, column=0)
+my_combobox.place(relwidth=0.6, relheight=0.05, relx=0.3, rely=0.4)
+
 
 # ----------------------button 1------------------------
 
-button1 = ttk.Button(my_window, text="Show Query Results",
+button1 = ttk.Button(my_window, text="Show Results  from DB1",
                      command=lambda: run_query())
-button1.grid(row=1, column=0)
-
-#---------------------------------------
+#button1.grid(row=2, column=0)
+button1.place(relwidth=0.28, relheight=0.06, relx=0.3, rely=0.5)
+#------------------checkbutton-----------------------
+#measureSystem = StringVar()
+button2 = ttk.Button(my_window, text='Show Results from both',
+	    command=run_query())
+button2.place(relwidth=0.3, relheight=0.06, relx=0.6, rely=0.5)
+# ---------------------------------------
 
 conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:/WSU FY2012 OP.mdb;')
 cursor = conn.cursor()
 
 # ------------------------SQL command--------------------------------------
-#example inputs to test the query:
+# example inputs to test the query:
 a_v_min = "V140"
 a_v_max = "V230"
 a_e_min = "E140"
@@ -109,44 +153,42 @@ OR
 )
 ORDER BY [FIPS County Code Table].[County Name]; '''
 
+# ------for the sake of time saving during test running, i comment this part for now:
 
+# cursor.execute(sql_command, (a_v_min, a_v_max, a_e_min, a_e_max, a_min, a_max))
 
-#------for the sake of time saving during test running, i comment this part for now:
-
-#cursor.execute(sql_command, (a_v_min, a_v_max, a_e_min, a_e_max, a_min, a_max))
-
-#for row in cursor.fetchall():
+# for row in cursor.fetchall():
 #    print(row)
-#--------------------------------------------------------
-gdf=gpd.read_file('GU_CountyOrEquivalent.shp')
+# remember to add a Progressbar later
+# --------------------------------------------------------
+gdf = gpd.read_file('GU_CountyOrEquivalent.shp')
 df = pd.DataFrame(gdf)
-df_seg=pd.read_csv("seg.csv")
-new_df = df[df["State_Name"] == 'Kansas' ]
+df_seg = pd.read_csv("seg.csv")
+new_df = df[df["State_Name"] == 'Kansas']
 new_df.reset_index(drop=True, inplace=True)
-merge_df=pd.merge(new_df, df_seg, left_on='County_Nam', right_on='County', how='inner')
+merge_df = pd.merge(new_df, df_seg, left_on='County_Nam', right_on='County', how='inner')
 
 gdf2 = gpd.GeoDataFrame(merge_df)
 
 gdf2['coords'] = gdf2['geometry'].apply(lambda x: x.representative_point().coords[:])
 gdf2['coords'] = [coords[0] for coords in gdf2['coords']]
 
-#print(gdf.shape)
-#print(gdf.head())
-#gdf2.plot(column='County_Nam')
+# print(gdf.shape)
+# print(gdf.head())
+# gdf2.plot(column='County_Nam')
 
 print(gdf2.head())
-plt.rcParams['figure.figsize']=(20,15)
+plt.rcParams['figure.figsize'] = (20, 15)
 ax = gdf2.plot(column='Segment')
-gdf2.apply(lambda x: ax.annotate(s=x.County_Nam, xy=x.geometry.centroid.coords[0], ha='center'),axis=1)
-plt.show()
+gdf2.apply(lambda x: ax.annotate(s=x.County_Nam, xy=x.geometry.centroid.coords[0], ha='center'), axis=1)
+# plt.show()
 
-seg2=merge_df[['County_Nam','Segment']]
-print(merge_df[['County_Nam','Segment']])
-seg2.to_csv('seg2_out.csv',encoding='utf-8')
-#new_df.to_csv('new-df.csv',encoding='utf-8')
+seg2 = merge_df[['County_Nam', 'Segment']]
+print(merge_df[['County_Nam', 'Segment']])
+seg2.to_csv('seg2_out.csv', encoding='utf-8')
+# new_df.to_csv('new-df.csv',encoding='utf-8')
 print(new_df.head())
 
-
-#----------------------running ----------------------
+# ----------------------running ----------------------
 
 my_window.mainloop()
